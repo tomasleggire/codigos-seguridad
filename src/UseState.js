6dbbc855abdf30f21dbbe7b54b86cfa34a24720a
Tomás Leggire <tomasleggire@hotmail.com>
2022-11-02 21:17:10 -0300

    Diferencia de estados entre UseState y ClassState

--- src/UseState.js
+++ src/UseState.js
@@ -1,13 +1,22 @@
 import React from "react";
 
-function UseState() {
+function UseState({name}) {
+    const [error, setError] = React.useState(false);
+
     return (
         <div>
-          <h2>Eliminar UseState</h2>
+          <h2>Eliminar {name}</h2>
           <p>Por favor, escribe el código de seguridad.</p>
+
+          {error && (
+            <p>Error: el código es incorrecto</p>
+          )}
+
           <input placeholder="Código de seguridad"/>
-          <button>Comprobar</button>
-        </div>
+          <button
+            onClick={() => setError(!error)}
+          >Comprobar</button>
+        </div> 
     )
 }
 
