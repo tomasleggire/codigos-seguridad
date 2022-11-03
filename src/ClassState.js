baf3cb484a3853473f1bde1867c584b82d72fcc7
Tomás Leggire <tomasleggire@hotmail.com>
2022-11-02 21:56:38 -0300

    loading con useEffect en UseState

--- src/ClassState.js
+++ src/ClassState.js
@@ -4,7 +4,7 @@ class ClassState extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            error: false,
+            error: true,
         };
     }
 
--- src/UseState.js
+++ src/UseState.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function UseState({name}) {
-    const [error, setError] = React.useState(false);
+    const [error, setError] = React.useState(true);
+    const [loading, setLoading] = React.useState(false);
+
+    React.useEffect(() => {
+        console.log('Empezando el efecto');
+        if (loading) {
+            setTimeout(() => {
+                console.log('Haciendo la validacion');
+                setLoading(false);
+                console.log('Terminando la validacion');
+            }, 3000);
+        }
+        console.log('Terminando el efecto');
+    }, [loading]);
 
     return (
         <div>
@@ -12,9 +25,13 @@ function UseState({name}) {
             <p>Error: el código es incorrecto</p>
           )}
 
+          {loading && (
+            <p>Cargando...</p>
+          )}
+
           <input placeholder="Código de seguridad"/>
           <button
-            onClick={() => setError(!error)}
+            onClick={() => setLoading(true)}
           >Comprobar</button>
         </div> 
     )
